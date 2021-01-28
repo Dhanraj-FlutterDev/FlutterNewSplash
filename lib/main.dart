c493d3f020da6ae83a2b27e32dc4ecda22c343ed
Dhanraj-MobileAppDev <52417093+DhanrajNilkanth@users.noreply.github.com>
2021-01-28 12:24:42 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,46 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';import 'package:flutter/material.dart';import 'package:page_transition/page_transition.dart';void main()=> runApp(MyApp());class MyApp extends StatelessWidget {  @override  Widget build(BuildContext context) {    return MaterialApp(      debugShowCheckedModeBanner: false,      home: AnimatedSplashScreen(          duration: 20000,          splash: 'assets/among.png',          nextScreen: MainScreen(),        splashIconSize: 350.0,        splashTransition: SplashTransition.scaleTransition,        pageTransitionType: PageTransitionType.downToUp,        backgroundColor: Color.fromRGBO(255, 222, 42,1),      ),    );  }}class MainScreen extends StatelessWidget {  @override  Widget build(BuildContext context) {    return Scaffold(      appBar: AppBar(title: Text('Home'),),      backgroundColor: Colors.blue.shade200,      body: Center(        child: Text('Main Screen',style: TextStyle(fontSize: 30),),      ),    );  }}
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+void main()=> runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AnimatedSplashScreen(
+          duration: 20000,
+          splash: 'assets/among.png',
+          nextScreen: MainScreen(),
+        splashIconSize: 350.0,
+        splashTransition: SplashTransition.scaleTransition,
+        pageTransitionType: PageTransitionType.downToUp,
+        backgroundColor: Color.fromRGBO(255, 222, 42,1),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home'),),
+      backgroundColor: Colors.blue.shade200,
+      body: Center(
+        child: Text('Main Screen',style: TextStyle(fontSize: 30),),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
